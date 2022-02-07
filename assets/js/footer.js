a27403a771611a42b4daa9e22a946d566e1c57c6
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2022-02-07 23:44:45 +0000

    become a sponsor and change color to white

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,24 +1,25 @@
 document.getElementById("footer").innerHTML =
 
 `
-<footer class="page-footer green lighten-2">
+<footer class="page-footer white z-depth-0">
+<div class="divider"></div>
 <div class="container">
   <div class="row">
     <div class="col s8">
-      <h5 class="white-text">Open Source Project</h5>
-      <p  class="white-text">Building the tool for a better and comprehensive design</p>
+      <h5 class="black-text">Open Source Project</h5>
+      <p  class="black-text">Building the tool for a better and comprehensive design</p>
     </div>
     <div class="col s4">
-      <h5 class="white-text">Connect</h5>
-      <i> <a  class="material-icons white-text" href="mailto:pellegrino.marco@icloud.com">mail</a> </i>
-      <i> <a  class="material-icons white-text" href="https://wa.me/393291536801">whatsapp</a> </i>
-      <i> <a  class="material-icons white-text" href="https://www.linkedin.com/in/mp0110/">work</a> </i>
+      <h5 class="black-text">Connect</h5>
+      <i> <a  class="material-icons black-text" href="mailto:pellegrino.marco@icloud.com">mail</a> </i>
+      <i> <a  class="material-icons black-text" href="https://wa.me/393291536801">whatsapp</a> </i>
+      <i> <a  class="material-icons black-text" href="https://www.linkedin.com/in/mp0110/">work</a> </i>
     </div>
   </div>
 </div>
 
 <div class="footer-copyright">
-  <div class="container white-text">
+  <div class="container black-text">
     Made with <a class= "orange-text" href="https://en.wikipedia.org/wiki/Love" target="_blank"> Love</a>
   </div>
 </div>
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,22 +2,22 @@ document.getElementById("navbar").innerHTML =
 `
 <div class="navbar-fixed">
     
-    <nav class="red darken-3">
+    <nav class="white z-depth-0">
       <div class="container">
-      <div class="nav-wrapper white">
+      <div class="nav-wrapper">
         <!-- <div class="container "> -->
-          <a class="sidenav-trigger" href="#" data-target="mobile-demo"><i class="material-icons white-text">menu</i></a>
-          <a class="brand-logo white-text" href="index.html">Alpaca4d</a>       
+          <a class="sidenav-trigger" href="#" data-target="mobile-demo"><i class="material-icons black-text">menu</i></a>
+          <a class="brand-logo black-text" href="index.html">Alpaca4d</a>       
           <ul class="right hide-on-med-and-down">
-            <li><a class="white-text" href="about.html">About</a></li>
-            <li><a class="white-text" href="download.html">Download</a></li>
-            <li><a class="white-text" href="examples.html">Examples</a></li>
+            <li><a class="black-text" href="about.html">About</a></li>
+            <li><a class="black-text" href="download.html">Download</a></li>
+            <li><a class="black-text" href="examples.html">Examples</a></li>
           </ul>
       </div>
       </div>
     </nav>
-  
   </div>
+
   
   
     <ul class="sidenav  indigo darken-2" id="mobile-demo">
