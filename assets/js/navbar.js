e3961bbd26a862586c3ce26895c7aa3d284b216b
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2022-02-08 23:47:39 +0000

    added Pulse

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -12,7 +12,7 @@ document.getElementById("navbar").innerHTML =
             <li><a class="black-text" href="about.html">About</a></li>
             <li><a class="black-text" href="download.html">Download</a></li>
             <li><a class="black-text" href="examples.html">Examples</a></li>
-            <li><a class="red-text text-darken-2" style="font-weight:600" href="become_a_sponsor.html">Sponsors</a></li>
+            <li><a class="red darken-2 white-text text-darken-2 btn pulse" style="font-weight:600; border-radius:10px;" href="become_a_sponsor.html">Sponsors</a></li>
           </ul>
       </div>
       </div>
@@ -25,6 +25,6 @@ document.getElementById("navbar").innerHTML =
       <li><a class="white-text" href="about.html">About</a></li>
       <li><a class="white-text" href="download.html">Download</a></li>
       <li><a class="white-text" href="examples.html">Examples</a></li>
+      <li><a class="orange-text text-darken-2" style="font-weight:600" href="become_a_sponsor.html">Sponsors</a></li>
     </ul>
-
 `
