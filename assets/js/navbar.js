88cc4e90ee74d2bf975e834d23107dd93c6adb78
Marco-Pellegrino <53620289+Marco-Pellegrino@users.noreply.github.com>
2022-11-01 22:51:40 +0100

    workshop is coming

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -13,6 +13,7 @@ document.getElementById("navbar").innerHTML =
             <li><a class="black-text" href="download.html">Download</a></li>
             <li><a class="black-text" href="examples.html">Examples</a></li>
             <li><a class="red darken-2 white-text text-darken-2 btn pulse" style="font-weight:600; border-radius:10px;" href="become_a_sponsor.html">Sponsors</a></li>
+            <li><a class="green darken-2 white-text text-darken-2 btn pulse" style="font-weight:600; border-radius:10px;" href="workshop.html">Workshop</a></li>
           </ul>
       </div>
       </div>
@@ -26,5 +27,6 @@ document.getElementById("navbar").innerHTML =
       <li><a class="white-text" href="download.html">Download</a></li>
       <li><a class="white-text" href="examples.html">Examples</a></li>
       <li><a class="orange-text text-darken-2" style="font-weight:600" href="become_a_sponsor.html">Sponsors</a></li>
+      <li><a class="orange-text text-darken-2" style="font-weight:600" href="workshop.html">Workshop</a></li>
     </ul>
 `
