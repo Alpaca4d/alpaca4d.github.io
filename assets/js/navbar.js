4506238b8d14635b4539136f568d4c11d40a416b
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2023-09-15 17:37:09 +0200

    :bug:

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -11,7 +11,7 @@ document.getElementById("navbar").innerHTML =
           <ul class="right hide-on-med-and-down">
             <li><a class="black-text" style="border-radius:20px;" href="./../about/index.html">About</a></li>
             <li><a class="black-text" style="border-radius:20px;" href="./../download.html">Download</a></li>
-            <li><a class="black-text" style="border-radius:20px;" href="https://alpaca4d.gitbook.io/docs/">Docs</a></li>
+            <li><a class="black-text" style="border-radius:20px;" href="https://alpaca4d.gitbook.io/docs">Docs</a></li>
             <li><a class="red darken-2 white-text text-darken-2 btn" style="font-weight:600; border-radius:10px;" href="./../become_a_sponsor.html">Sponsors</a></li>
             <li><a class="green darken-2 white-text text-darken-2 btn" style="font-weight:600; border-radius:10px;" href="./../workshop.html">Workshop</a></li>
           </ul>
@@ -25,7 +25,7 @@ document.getElementById("navbar").innerHTML =
     <ul class="sidenav  indigo darken-2" id="mobile-demo">
       <li><a class="white-text" href="./../about/index.html">About</a></li>
       <li><a class="white-text" href="./../download.html">Download</a></li>
-      <li><a class="white-text" href="https://alpaca4d.gitbook.io/docs/">Docs</a></li>
+      <li><a class="white-text" href="https://alpaca4d.gitbook.io/docs">Docs</a></li>
       <li><a class="orange-text text-darken-2" style="font-weight:600" href="./../become_a_sponsor.html">Sponsors</a></li>
       <li><a class="orange-text text-darken-2" style="font-weight:600" href="./../workshop.html">Workshop</a></li>
     </ul>
