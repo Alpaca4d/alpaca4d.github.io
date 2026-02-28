a90222da17b5832069dd206eb413196dfb9619d5
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2026-02-28 20:57:11 +0100

    🆙 buy instruction

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -12,8 +12,8 @@ document.getElementById("navbar").innerHTML =
             <li><a class="black-text btn-flat waves-effect" style="border-radius:10px;" href="./../about/index.html">About</a></li>
             <li><a class="black-text btn-flat waves-effect" style="border-radius:10px;" href="./../download.html">Download</a></li>
             <li><a class="black-text btn-flat waves-effect" style="border-radius:10px;" href="https://alpaca4d.gitbook.io/docs">Docs</a></li>
-            <!-- <li><a class="green darken-2 white-text text-darken-2 btn" style="font-weight:600; border-radius:10px;" href="./../buy.html">Buy</a></li> -->
-            <li><a class="red darken-2 white-text text-darken-2 btn" style="font-weight:600; border-radius:10px;" href="./../become_a_sponsor.html">Sponsors</a></li>
+            <li><a class="red darken-2 white-text text-darken-2 btn" style="font-weight:600; border-radius:10px;" href="./../buy.html">Buy</a></li>
+            <!-- <li><a class="red darken-2 white-text text-darken-2 btn" style="font-weight:600; border-radius:10px;" href="./../become_a_sponsor.html">Sponsors</a></li> -->
         <li><a class="green darken-2 white-text text-darken-2 btn" style="font-weight:600; border-radius:10px;" href="./../workshop.html">Workshop</a></li>
           </ul>
       </div>
@@ -27,8 +27,8 @@ document.getElementById("navbar").innerHTML =
       <li><a class="white-text" href="./../about/index.html">About</a></li>
       <li><a class="white-text" href="./../download.html">Download</a></li>
       <li><a class="white-text" href="https://alpaca4d.gitbook.io/docs">Docs</a></li>
-      <!-- <li><a class="green-text text-darken-2" style="font-weight:600" href="./../buy.html">Buy</a></li> -->
-      <li><a class="orange-text text-darken-2" style="font-weight:600" href="./../become_a_sponsor.html">Sponsors</a></li>
+      <li><a class="orange-text text-darken-2" style="font-weight:600" href="./../buy.html">Buy</a></li>
+      <!-- <li><a class="orange-text text-darken-2" style="font-weight:600" href="./../become_a_sponsor.html">Sponsors</a></li> -->
       <li><a class="green-text text-darken-2" style="font-weight:600" href="./../workshop.html">Workshop</a></li>
     </ul>
 `
